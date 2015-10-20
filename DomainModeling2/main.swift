1c7e2808100e577a98f994f32848558cf86e68e4
Teddy Pappas <mbteddy@verizon.net>
2015-10-20 14:58:30 -0700

    first commit

---
+++ DomainModeling2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DomainModeling2
+//
+//  Created by Teddy Pappas on 10/20/15.
+//  Copyright © 2015 Teddy Pappas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
